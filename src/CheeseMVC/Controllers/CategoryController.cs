a2a15c356c5aa31624095b0959ad8d8d55518262
Bridgette Kennedy <bridgetteakennedy@gmail.com>
2019-12-15 13:28:20 -0500

    Task 1 Complete

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<CheeseCategory> cats = context.Categories.ToList();
+            return View(cats);
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel) {
+            if (ModelState.IsValid)
+            {
+                CheeseCategory newCategory = new CheeseCategory
+                {
+                    Name = addCategoryViewModel.Name,
+                };
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+                return Redirect("Index");
+
+            };
+
+                return View();
+            }
+    }
+    }
+    }
